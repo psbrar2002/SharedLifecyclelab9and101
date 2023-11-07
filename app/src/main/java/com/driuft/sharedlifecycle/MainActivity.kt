c8a74eff930d852828b4aa05ba718210a1c4f887
Prabhdeep Brar <psbrar2002@gmail.com>
2023-11-06 22:00:34 -0800

    lab9 completed

--- app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
+++ app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
@@ -1,21 +1,52 @@
 package com.driuft.sharedlifecycle
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var _sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _sharedPreferences = this.getSharedPreferences(this.packageName, Context.MODE_PRIVATE)
+
+        with (_sharedPreferences.edit()) {
+            // replace "onCreate" with the name of the method you're saving
+            putString(getString(R.string.current_lifecycle_state), "onCreate")
+            apply()
+        }
+        // Read the state
+        val currentState = _sharedPreferences.getString(
+            getString(R.string.current_lifecycle_state),
+            resources.getString(R.string.default_state)
+        ) ?: resources.getString(R.string.default_state)
+        // Log statement at debug level
+        // Log the state
+        Log.d("Lifecycle: ", currentState)
+        // Toast the state
+        Toast.makeText(this, currentState, Toast.LENGTH_SHORT).show()
     }
 
     override fun onStart() {
         super.onStart()
+        with (_sharedPreferences.edit()) {
+            putString(getString(R.string.current_lifecycle_state), "onStart")
+            apply()
+        }
+
+        Log.d("Lifecycle: ", "onStart")
+        // Toast
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("Lifecycle: ", "onResume")
+        // Toast
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
